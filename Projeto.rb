9899d515f762202cb350459cdde0cbea9b9a9a81
Micaelsow <micaellimavieira@gmail.com>
2025-07-28 21:57:43 -0300

    Update Projeto.rb

--- Projeto.rb
+++ Projeto.rb
@@ -5,5 +5,8 @@ decricao = gets.chomp
 puts "Qual a quantidade inicial em estoque? (Obrigatório)"
 quantidade = gets.chomp.to_i
 puts "Qual o preço do produto? (Opcional)"
-  preco = gets.chomp.to_f
+preco = gets.chomp.to_f
 
+puts "Produto: #{produto}"
+puts "Descrição: #{decricao}"
+puts "Quantidade em estoque: #{quantidade}"
