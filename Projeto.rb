722320f74e6cd770e770a68419d48a50ff05eed7
Micaelsow <micaellimavieira@gmail.com>
2025-07-28 21:59:39 -0300

    Update Projeto.rb

--- Projeto.rb
+++ Projeto.rb
@@ -9,10 +9,14 @@ puts "Escreva a descrição do produto(Opcional):"
 decricao = gets.chomp
 puts "Qual a quantidade inicial em estoque? (Obrigatório)"
 quantidade = gets.chomp
-
+if quantidade == ""
+  puts "O campo tem de ser obrigatoriamente preenchido"
+  exit
+else
+end
 puts "Qual o preço do produto? (Opcional)"
 preco = gets.chomp.to_f
 
 puts "Produto: #{produto}"
 puts "Descrição: #{decricao}"
-puts "Quantidade em estoque: #{quantidade}"
+puts "Quantidade em estoque: #{quantidade.to_i}"
