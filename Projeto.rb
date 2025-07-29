4d5bd323d66b95b6a3ab90601d4d895a20990b6c
Micaelsow <micaellimavieira@gmail.com>
2025-07-28 21:58:41 -0300

    Update Projeto.rb

--- Projeto.rb
+++ Projeto.rb
@@ -1,5 +1,10 @@
 puts "Escreva o nome do produto(Obrigatório):"
 produto = gets.chomp
+if produto == ""
+  puts "O campo tem de ser obrigatoriamente preenchido"
+  exit
+else
+end
 puts "Escreva a descrição do produto(Opcional):"
 decricao = gets.chomp
 puts "Qual a quantidade inicial em estoque? (Obrigatório)"
