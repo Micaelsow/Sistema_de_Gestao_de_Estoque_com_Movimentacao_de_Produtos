a4bda0a56b47775d9d8b5e7a528a77be6825d94b
Micaelsow <micaellimavieira@gmail.com>
2025-07-28 21:54:56 -0300

    Update Projeto.rb

--- Projeto.rb
+++ Projeto.rb
@@ -1,6 +1,9 @@
 puts "Escreva o nome do produto(Obrigatório):"
 produto = gets.chomp
-puts "Escreva a descrição do produto(Opicional):"
+puts "Escreva a descrição do produto(Opcional):"
 decricao = gets.chomp
 puts "Qual a quantidade inicial em estoque? (Obrigatório)"
 quantidade = gets.chomp.to_i
+puts "Qual o preço do produto? (Opcional)"
+  preco = gets.chomp.to_f
+
