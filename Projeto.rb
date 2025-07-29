f2feb01676848405f39850570efb0bad63f2d06f
Micaelsow <micaellimavieira@gmail.com>
2025-07-28 22:02:25 -0300

    Update Projeto.rb

--- Projeto.rb
+++ Projeto.rb
@@ -3,7 +3,6 @@ produto = gets.chomp
 if produto == ""
   puts "O campo tem de ser obrigatoriamente preenchido"
   exit
-else
 end
 puts "Escreva a descrição do produto(Opcional):"
 decricao = gets.chomp
@@ -12,7 +11,6 @@ quantidade = gets.chomp
 if quantidade == ""
   puts "O campo tem de ser obrigatoriamente preenchido"
   exit
-else
 end
 puts "Qual o preço do produto? (Opcional)"
 preco = gets.chomp.to_f
