4ec649217943aef88e2d205a64638ef0e7cbc318
Micaelsow <micaellimavieira@gmail.com>
2025-08-04 10:43:17 -0300

    Update Projeto.rb

--- Projeto.rb
+++ Projeto.rb
@@ -1,19 +1,43 @@
+produtos = []
+
 loop do
   puts "Escreva o nome do produto(Obrigatório):"
-  produto = gets.chomp.strip
-  if produto == ""
+  nome = gets.chomp.strip
+  if nome.empty?
     puts "O nome do produto é obrigatório. Tente novamente."
       next
     end
+
   puts "Escreva a descrição do produto(Opcional):"
   descricao = gets.chomp.strip
+
   puts "Qual a quantidade inicial em estoque? (Obrigatório)"
   quantidade = gets.chomp.strip
   if quantidade.empty? || quantidade.to_i.to_s != quantidade
     puts "A quantidade inicial é obrigatória e deve ser um número inteiro. Tente novamente"
     next
   end
+
   puts "Qual o preço do produto? (Opcional)"
   preco = gets.chomp.to_f
-  break
+
+  produto = {
+    nome: nome,
+    descricao: descricao,
+    quantidade: quantidade.to_i,
+    preco: preco
+  }
+
+  produtos << produto
+puts "Produto cadastrado com sucesso"
+
+break
+end
+
+produtos.each_with_index do |produto, index|
+  puts "Produto #{index + 1}"
+  puts " Nome: #{produto[:nome]}"
+  puts " Descrição: #{produto[:descricao].empty? ? 'Sem descrição' : produto[:descricao]}"
+  puts " Quantidade em estoque: #{produto[:quantidade]}"
+
 end
