3bb65f984447fd21ff2e96bba4f52409eebdb1d8
Micaelsow <micaellimavieira@gmail.com>
2025-08-04 10:16:40 -0300

    Update Projeto.rb

--- Projeto.rb
+++ Projeto.rb
@@ -1,17 +1,17 @@
 loop do
   puts "Escreva o nome do produto(Obrigatório):"
-  produto = gets.chomp
+  produto = gets.chomp.strip
   if produto == ""
     puts "O nome do produto é obrigatório. Tente novamente."
-      break
+      next
     end
   puts "Escreva a descrição do produto(Opcional):"
-  decricao = gets.chomp
+  descricao = gets.chomp.strip
   puts "Qual a quantidade inicial em estoque? (Obrigatório)"
-  quantidade = gets.chomp
-  if quantidade == ""
-    puts "A quantidade inicial é obrigatória. Tente novamente"
-    break
+  quantidade = gets.chomp.strip
+  if quantidade.empty? || quantidade.to_i.to_s != quantidade
+    puts "A quantidade inicial é obrigatória e deve ser um número inteiro. Tente novamente"
+    next
   end
   puts "Qual o preço do produto? (Opcional)"
   preco = gets.chomp.to_f
