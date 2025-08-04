ae9ca8307f3a801be53c63e38f8e6a17b06b7b97
Micaelsow <micaellimavieira@gmail.com>
2025-08-04 10:05:19 -0300

    Update Projeto.rb

--- Projeto.rb
+++ Projeto.rb
@@ -1,22 +1,19 @@
-puts "Escreva o nome do produto(Obrigatório):"
-produto = gets.chomp
-puts "Escreva a descrição do produto(Opcional):"
-decricao = gets.chomp
-puts "Qual a quantidade inicial em estoque? (Obrigatório)"
-quantidade = gets.chomp
-puts "Qual o preço do produto? (Opcional)"
-preco = gets.chomp.to_f
-
 loop do
-  puts produto
+  puts "Escreva o nome do produto(Obrigatório):"
+  produto = gets.chomp
   if produto == ""
-    puts "O campo tem de ser obrigatoriamente preenchido"
-    break
-  end
-  puts decricao
-  puts quantidade
+    puts "O nome do produto é obrigatório. Tente novamente."
+      break
+    end
+  puts "Escreva a descrição do produto(Opcional):"
+  decricao = gets.chomp
+  puts "Qual a quantidade inicial em estoque? (Obrigatório)"
+  quantidade = gets.chomp
   if quantidade == ""
-    puts "O campo tem de ser obrigatoriamente preenchido"
+    puts "A quantidade inicial é obrigatória. Tente novamente"
     break
   end
+  puts "Qual o preço do produto? (Opcional)"
+  preco = gets.chomp.to_f
+  break
 end
